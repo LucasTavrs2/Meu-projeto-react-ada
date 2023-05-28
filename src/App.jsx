4e67a8c8d9bff3225fcfb73d543801527c4a729d
Bowie <marketing@letscode.com.br>
2023-05-28 17:30:39 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+
+import MeuContador from "./MeuContador"
+  
+/*
+import MinhaList from "./components/MinhaLista";
+*/
+/*
+import { useEffect, useState } from "react"
+
+const minhaLista = [
+  {id: '1', value: 'Fruta'},
+  {id: '2', value: 'Legume'},
+  {id: '3', value: 'carne'},
+]
+*/
+
+
+
+export default function App() {
+  
+  return (
+    
+    <div>
+      <MeuContador />
+    </div>
+    
+  )
+}
+
---
+++ src/components/MeuComponente.jsx
@@ -0,0 +1,11 @@
+export default function MeuComponente(){
+    return(
+      <p>Lorem ipsum dolor sit amet consectetur 
+        adipisicing elit. Nesciunt dolorem excepturi magni,
+         quos veritatis, corrupti recusandae, vero quod commodi voluptas doloribus placeat iure explicabo?
+          Animi, quo. Ut provident optio accusantium!</p>
+    )
+  }
+
+
+
